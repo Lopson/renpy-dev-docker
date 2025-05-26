274e3d9baa022895ccd89386a11917838bc5c445
Lopson <gmdlourenco@gmail.com>
2025-05-26 23:04:15 +0200

    Locale support at Powershell level

--- ContainerManagement.ps1
+++ ContainerManagement.ps1
@@ -6,7 +6,50 @@ function Test-ContainerName {
     [OutputType([bool])]
     param([Parameter(Mandatory = $true)][ValidateNotNullOrEmpty()][string]$Container)
     
+    if (-not ($Variables.images)) {
+        throw New-Object System.MemberAccessException(
+            "Variables file does not contain list of images"
+        );
+    }
+
     if (-not ($Variables.images -contains $Container)) {
+        return $false;
+    }
+
+    return $true;
+}
+
+function Test-ContainerLocale {
+    # NOTE: Testing the locale + sublocale combo is up to the container.
+    # There's no way for us to determine that at this level.
+
+    [OutputType([bool])]
+    param(
+        [ValidateNotNullOrEmpty()][string]$Locale,
+        [string]$Sublocale
+    )
+
+    if (-not [string]::IsNullOrWhiteSpace($Locale)) { $Locale = $Locale.ToLower(); }
+    if (-not [string]::IsNullOrWhiteSpace($Sublocale)) { $Sublocale = $Sublocale.ToUpper() };
+
+    if (-not ($Variables.locales)) {
+        throw New-Object System.MemberAccessException(
+            "Variables file does not contain list of supported locales"
+        );
+    }
+
+    [string[]]$LocalesList = $Variables.locales | ForEach-Object {
+        if ($_.Length -gt 1) { $_[0].ToLower() } };
+    [string[]]$SublocalesList = $Variables.locales | ForEach-Object {
+        if ($_.Length -eq 2) { $_[1].ToUpper(); } };
+
+    if (-not [string]::IsNullOrWhiteSpace($Locale) -and `
+            -not ($LocalesList -contains $Locale)) {
+        return $false;
+    }
+
+    if (-not [string]::IsNullOrWhiteSpace($Sublocale) -and `
+            -not ($SublocalesList -contains $Sublocale)) {
         return $false;
     }
 
@@ -15,7 +58,12 @@ function Test-ContainerName {
 
 function Test-VolumePath {
     [OutputType([bool])]
-    param([Parameter(Mandatory = $true)][AllowNull()][AllowEmptyString()][string]$Volume)
+    param(
+        [Parameter(Mandatory = $true)]
+        [AllowNull()]
+        [AllowEmptyString()]
+        [string]$Volume
+    )
     
     if ([string]::IsNullOrWhiteSpace($Volume)) {
         return $false;
@@ -25,21 +73,32 @@ function Test-VolumePath {
 
 function Get-ComposePath {
     [OutputType([string])]
-    param([Parameter(Mandatory = $true)][ValidateScript({Test-ContainerName -Container $_})][string]$Container)
+    param(
+        [Parameter(Mandatory = $true)]
+        [ValidateScript({ Test-ContainerName -Container $_ })]
+        [string]$Container
+    )
     
     return "$PSScriptRoot$([IO.Path]::DirectorySeparatorChar)$Container$([IO.Path]::DirectorySeparatorChar)compose.yml";
 }
 
 function Get-DockerfilePath {
     [OutputType([string])]
-    param([Parameter(Mandatory = $true)][ValidateScript({Test-ContainerName -Container $_})][string]$Container)
+    param(
+        [Parameter(Mandatory = $true)]
+        [ValidateScript({ Test-ContainerName -Container $_ })]
+        [string]$Container
+    )
     
     return "$PSScriptRoot$([IO.Path]::DirectorySeparatorChar)$Container$([IO.Path]::DirectorySeparatorChar)Dockerfile";
 }
 
 function Get-EnvFilePath {
     [OutputType([string])]
-    param([Parameter(Mandatory = $true)][ValidateScript({Test-ContainerName -Container $_})][string]$Container)
+    param(
+        [Parameter(Mandatory = $true)]
+        [ValidateScript({ Test-ContainerName -Container $_ })][string]$Container
+    )
 
     return "$([System.IO.Path]::GetTempPath())$Container.env";
 }
@@ -47,7 +106,11 @@ function Get-EnvFilePath {
 function Initialize-RenpyContainer {
     [OutputType([System.Void])]
     param (
-        [Parameter(Mandatory = $true)][ValidateScript({Test-ContainerName -Container $_})][string]$Container,
+        [Parameter(Mandatory = $true)]
+        [ValidateScript({ Test-ContainerName -Container $_ })][string]$Container,
+        [Parameter(Mandatory = $true)]
+        [ValidateScript({ Test-ContainerLocale -Container $_ })][string]$Locale,
+        [ValidateScript({ Test-ContainerLocale -Sublocale $_ })][string]$Sublocale,
         [string]$Volume
     )
 
@@ -57,6 +120,8 @@ function Initialize-RenpyContainer {
 
     [System.Text.StringBuilder]$EnvFile = [System.Text.StringBuilder]::new();
     $EnvFile.Append("DOCKER_MOUNT=`"$Volume`"") > $null;
+    $EnvFile.Append("LOCALE=`"$Locale`"") > $null;
+    $EnvFile.Append("SUBLOCALE=`"$Sublocale`"") > $null;
 
     [string]$EnvFilePath = $(Get-EnvFilePath -Container $Container);
     Set-Content -Value $EnvFile.ToString() -Encoding "utf8BOM" -LiteralPath $EnvFilePath;
@@ -66,19 +131,23 @@ function Initialize-RenpyContainer {
 function Start-RenpyContainer {
     [OutputType([System.Void])]
     param (
-        [Parameter(Mandatory = $true)][ValidateScript({Test-ContainerName -Container $_})][string]$Container,
-        [string]$Volume
+        [Parameter(Mandatory = $true)]
+        [ValidateScript({ Test-ContainerName -Container $_ })]
+        [string]$Container
     )
     
-    Initialize-RenpyContainer -Container $Container -Volume $Volume;
     [string]$EnvFilePath = $(Get-EnvFilePath -Container $Container);
-
-    docker compose --file $(Get-ComposePath -Container $Container) --env-file $EnvFilePath up --detach;
+    docker compose --file $(Get-ComposePath -Container $Container) `
+        --env-file $EnvFilePath up --detach;
 }
 
 function Stop-RenpyContainer {
     [OutputType([System.Void])]
-    param ([Parameter(Mandatory = $true)][ValidateScript({Test-ContainerName -Container $_})][string]$Container)
+    param (
+        [Parameter(Mandatory = $true)]
+        [ValidateScript({ Test-ContainerName -Container $_ })]
+        [string]$Container
+    )
 
     [string]$EnvFilePath = $(Get-EnvFilePath -Container $Container);
     if (-not $(Test-Path -LiteralPath $EnvFilePath)) {
@@ -87,27 +156,39 @@ function Stop-RenpyContainer {
         );
     }
 
-    docker compose --file $(Get-ComposePath -Container $Container) --env-file $EnvFilePath down;
-    Remove-Item -LiteralPath $EnvFilePath;
+    docker compose --file $(Get-ComposePath -Container $Container) `
+        --env-file $EnvFilePath down;
+    # Remove-Item -LiteralPath $EnvFilePath;
 }
 
 function Build-RenpyImage {
     [OutputType([System.Void])]
     param (
-        [Parameter(Mandatory = $true)][ValidateScript({Test-ContainerName -Container $_})][string]$Container,
+        [Parameter(Mandatory = $true)]
+        [ValidateScript({ Test-ContainerName -Container $_ })]
+        [string]$Container,
+        [Parameter(Mandatory = $true)]
+        [ValidateScript({ Test-ContainerLocale -Container $_ })][string]$Locale,
+        [ValidateScript({ Test-ContainerLocale -Sublocale $_ })][string]$Sublocale,
         [string]$Volume
     )
 
-    Initialize-RenpyContainer -Container $Container -Volume $Volume;
+    Initialize-RenpyContainer -Container $Container -Volume $Volume `
+        -Locale $Locale -Sublocale $Sublocale;
     [string]$EnvFilePath = $(Get-EnvFilePath -Container $Container);
 
-    docker compose --file $(Get-ComposePath -Container $Container) --env-file $EnvFilePath build;
-    Remove-Item -LiteralPath $EnvFilePath;
+    docker compose --file $(Get-ComposePath -Container $Container) `
+        --env-file $EnvFilePath build;
+    # Remove-Item -LiteralPath $EnvFilePath;
 }
 
 function Connect-RenpyContainer {
     [OutputType([System.Void])]
-    param ([Parameter(Mandatory = $true)][ValidateScript({Test-ContainerName -Container $_})][string]$Container)
+    param (
+        [Parameter(Mandatory = $true)]
+        [ValidateScript({ Test-ContainerName -Container $_ })]
+        [string]$Container
+    )
 
     docker exec -it "$ContainerPrefix$Container" bash;
 }
--- fedora/fedora_glx_xdg.dockerfile
+++ fedora/fedora_glx_xdg.dockerfile
@@ -3,3 +3,7 @@ RUN dnf install glx-utils xdg-utils libXrender libICE libSM libXi libXmu \
 libXt langpacks-ja langpacks-zh_CN  glibc-locale-source glibc-langpack-en \
 nano -y
 COPY ./common /etc/profile.d/
+SHELL ["/bin/bash", "-c"]
+ARG DESIRED_LOCALE
+ARG DESIRED_SUBLOCALE
+RUN source /etc/profile; dockerfile_set_locale ${DESIRED_LOCALE} ${DESIRED_SUBLOCALE}
